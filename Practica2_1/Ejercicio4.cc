363c799a4ad313bc70919e11ff1f4244b80e6c0b
pamart24 <pamart24@ucm.es>
2021-05-14 17:43:58 +0200

    Ejercicio 4

--- Practica2_1/Ejercicio4.cc
+++ Practica2_1/Ejercicio4.cc
@@ -9,8 +9,8 @@
 const size_t MAXBUFFER = 80;
 
 int main(int argc, char** argv) {
-	if (argc != 4) {
-		std::cerr << "Uso: ./Ejercicio3 [IP] [puerto] [comando]\n";
+	if (argc != 3) {
+		std::cerr << "Uso: ./Ejercicio3 [IP] [puerto]\n";
 		return -1;
 	}
 
@@ -36,21 +36,51 @@ int main(int argc, char** argv) {
 		return -1;
 	}
 
-	freeaddrinfo(res);
-
-	char buffer[MAXBUFFER];
-
-	if (sendto(sd, argv[3], strlen(argv[3]) + 1, 0, res->ai_addr, res->ai_addrlen) == -1) {
-		std::cerr << "ERROR: fallo en [sendTo]\n";
+	if (bind(sd, res->ai_addr, res->ai_addrlen) == -1) {
+		std::cerr << "ERROR: fallo en la asignacion de addr a socket\n";
 		return -1;
 	}
 
-	if (recvfrom(sd, buffer, MAXBUFFER - 1, 0, res->ai_addr, &res->ai_addrlen) == -1) {
-		std::cerr << "ERROR: fallo en [recvfrom]\n";      
-                return -1;
-        }
-	std::cout << buffer << "\n";
+	freeaddrinfo(res);
 
+	if (listen(sd, 1) == -1) {
+		std::cerr << "ERROR: fallo en listen\n";
+		return -1;
+	}
+
+	bool activo = true;
+	char buffer[MAXBUFFER];
+	char host[NI_MAXHOST];
+	char serv[NI_MAXSERV];
+
+	struct sockaddr cliente;
+	socklen_t clientelen = sizeof(struct sockaddr);
+	int clientesd = accept(sd, (struct sockaddr *) &cliente, &clientelen);
+
+	if (clientesd == -1) {
+                std::cerr << "ERROR: fallo en accept\n";
+                return -1;
+	}
+
+	getnameinfo(&cliente, clientelen, host, NI_MAXHOST, serv, NI_MAXSERV,
+		NI_NUMERICHOST | NI_NUMERICSERV);
+	std::cout << "Conexión desde " << host << " " << serv << "\n";
+	
+	while (activo) {
+		int bytes = recvfrom(clientesd, (void *) buffer, MAXBUFFER - 1, '\0',
+			&cliente, &clientelen);
+		if (bytes < 1) {
+			std::cout << "Conexión terminada\n";
+			activo = false;
+		}
+		buffer[bytes] = '\0';	
+
+		//Hacer eco al cliente de lo que se recibe
+		sendto(clientesd, buffer, bytes, 0, &cliente, clientelen);
+		//send(clientesd, (void *) buffer, bytes, 0);
+	}
+
+	close(clientesd);
 	close(sd);
 	return 0;
 }
